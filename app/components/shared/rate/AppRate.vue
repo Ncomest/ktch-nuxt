3e825d312a929f6e1c2b565932d9620fa1873105
Hurtik Igor <hurtik.igor@gmail.com>
2025-09-19 19:40:44 +0300

    update: Обновлен раздел отзывов

---
+++ app/components/shared/rate/AppRate.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  value: {
+    type: Number,
+    required: true,
+    validator: (value) => value >= 0 && value <= 5
+  },
+  maxStars: {
+    type: Number,
+    default: 5
+  }
+})
+</script>
+
+<template>
+  <div class="app-rate">
+    <span
+        v-for="star in maxStars"
+        :key="star"
+        class="star"
+        :class="{ 'filled': star <= value }"
+    >
+      {{ star <= value ? '★' : '☆' }}
+    </span>
+  </div>
+</template>
+
+
+
+<style src="./style.scss">
+
+</style>
--- app/components/shared/review/TheReview.vue
+++ app/components/shared/review/TheReview.vue
@@ -1,5 +1,7 @@
 <script setup>
 import ImageWrapper from "@components/shared/image-wrapper/ImageWrapper.vue";
+import noimage from "/images/reviews/noimage.jpg"
+import AppRate from "@components/shared/rate/AppRate.vue";
 
 defineProps({
   item: { type: Object, required: true },
@@ -7,8 +9,10 @@ defineProps({
 </script>
 <template>
   <div class="the-review">
-    <ImageWrapper :src="item.ktch" :alt="item.name" />
+    <ImageWrapper :src="item.image || noimage" :alt="item.name" />
     <h3 class="the-review__name">{{ item.name }}</h3>
+    <app-rate :value="item.rate" />
+    <p>{{ item.date }}</p>
     <h5 class="the-review__price">Стоимость: {{ item.price.toLocaleString('ru-RU') }}₽</h5>
     <p class="the-review__text">{{ item.review }}</p>
   </div>
