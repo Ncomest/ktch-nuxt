8bccea62d179ff6891678e77e24c7cf8ae0233bd
Hurtik Igor <hurtik.igor@gmail.com>
2025-11-18 20:07:49 +0300

    Добавлен новый инпут, добавлена страница с редактором карточек кухонь

---
+++ app/components/shared/input/AppInput.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  title: { type: String },
+});
+const model = defineModel();
+</script>
+
+<template>
+  <div>
+    <label>{{ title }}</label>
+    <input v-model="model" />
+  </div>
+</template>
+
+<style src="./AppInput.scss"></style>
